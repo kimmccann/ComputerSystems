0279a671a56a54c2df52288d79b346bef7fd8747
Jamie Mac <development@jgmsoftware.co.uk>
2015-02-11 23:42:47 +0000

    yay..

--- ButtonGame/ButtonGame/Form1.cs
+++ ButtonGame/ButtonGame/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Timers;
+using System.Threading;
 
 namespace ButtonGame
 {
@@ -16,13 +17,13 @@ namespace ButtonGame
         Button[,] buttons = new Button[3, 3];
         Random r = new Random();
         List<Tuple<int, int>> sequence = new List<Tuple<int, int>>();
-        System.Timers.Timer t = new System.Timers.Timer();
-        System.Timers.Timer t2 = new System.Timers.Timer();
+        System.Timers.Timer timer1 = new System.Timers.Timer();
 
         public Form1()
         {
             InitializeComponent();
             //Creates our 3 by 3 grid off buttons
+
             for (int i = 0; i < buttons.GetLength(0); i++)
             {
                 for (int j = 0; j < buttons.GetLength(1); j++)
@@ -36,19 +37,20 @@ namespace ButtonGame
                     buttons[i, j].Text = Convert.ToString((i + 1) + "," + (j + 1));
                     buttons[i, j].Click += new EventHandler(this.butt_Click);
                     buttons[i, j].BackColor = Color.MediumTurquoise;
-                    this.Controls.Add(buttons[i, j]);    
+                    this.Controls.Add(buttons[i, j]);
                 }
             }
+            sequenceButtons();
+        }
+
+        public void sequenceButtons()
+        {
             sequence.Add(new Tuple<int, int>(r.Next(3), r.Next(3)));
-
-            //Creats our timer and it's events
-            t.Interval = 5000;
-            t2.Interval = 7500;
-            t.Enabled = true;
-            t2.Enabled = true;
-            t.Elapsed += TimerEvent;
-            t2.Elapsed += TimerEvent2;
-
+            Console.WriteLine("bla");
+            //Creats our timers and it's events
+            timer1.Interval = 5000;
+            timer1.Enabled = true;
+            timer1.Elapsed += TimerEvent;
         }
 
             
@@ -57,34 +59,34 @@ namespace ButtonGame
         {
             Console.WriteLine(((Button)sender).Text);
 
-            if (((Button)sender) == buttons[sequence[0].Item1, sequence[0].Item2])
+            for (int i = 0; i < sequence.Count; i++)
             {
-                //buttons[r.Next(3), r.Next(3)].BackColor = Color.Red;
-                Console.WriteLine("HIT!");
+                Console.Write(sequence.Count);
+                if (((Button)sender) == buttons[sequence[i].Item1, sequence[i].Item2])
+                {
+                    //If the user is right, add another item to the sequence.
+                    Console.WriteLine("HIT!");
+                    sequenceButtons();
+                }
+                else
+                    Console.WriteLine("MISSED!");
             }
-            else
-                Console.WriteLine("MISSED!");
         }
 
         private void TimerEvent(object sender, EventArgs e)
         {
-            foreach (var element in sequence)
+            for (int k = 0; k < sequence.Count; k++)
             {
-                buttons[sequence[0].Item1, sequence[0].Item2].BackColor = Color.Red;
+                Console.Write(sequence.Count);
+                Console.Write("sequence is " + sequence[k]);
+                buttons[sequence[k].Item1, sequence[k].Item2].BackColor = Color.Red;
+                Thread.Sleep(5000);
+                buttons[sequence[k].Item1, sequence[k].Item2].BackColor = Color.MediumTurquoise;
             }
             //Stops our button from repeatedly turning Red by ending the timer.
-            t.Stop();
+            timer1.Enabled = false;
         }
 
-        private void TimerEvent2(object sender, EventArgs e)
-        {
-            foreach (var element in sequence)
-            {
-                buttons[sequence[0].Item1, sequence[0].Item2].BackColor = Color.MediumTurquoise;
-            }
-            //Stops our button from repeatedly turning turquoise by ending the timer.
-            t2.Stop();
-        }
 
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
