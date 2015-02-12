c5fba5f552798c7a97d9a0a6f2bfbca58926111a
Kim <kimberleylmccann@gmail.com>
2015-02-12 22:13:55 +0000

    Fixed Merging and Colours, etc

--- ButtonGame/ButtonGame/Form1.cs
+++ ButtonGame/ButtonGame/Form1.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,136 +8,93 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Timers;
-using System.Threading;
 
-namespace WindowsFormsApplication1
+namespace ButtonGame
 {
     public partial class Form1 : Form
     {
-
-        Button[,] b = new Button[3, 3];
+        Button[,] buttons = new Button[3, 3];
         Random r = new Random();
-        Button start = new Button();
         List<Tuple<int, int>> sequence = new List<Tuple<int, int>>();
-        int score = 0;
+        System.Timers.Timer t = new System.Timers.Timer();
+        System.Timers.Timer t2 = new System.Timers.Timer();
 
         public Form1()
         {
             InitializeComponent();
-
-
-            for (int i = 0; i < 3; i++)
+            //Creates our 3 by 3 grid off buttons
+            for (int i = 0; i < buttons.GetLength(0); i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < buttons.GetLength(1); j++)
                 {
-                    b[i, j] = new Button();
-                    b[i, j].Name = "button" + (j + 1) + "," + (i + 1);
-                    b[i, j].Text = Convert.ToString((j + 1) + "," + (i + 1));
-                    b[i, j].Click += new EventHandler(this.Button_Click);
-                    b[i, j].Location = new Point(105 * (i + 1), 105 * (j + 1));
-                    b[i, j].Height = 100;
-                    b[i, j].Width = 100;
-                    b[i, j].BackColor = Color.MediumTurquoise;
-                    this.Controls.Add(b[i, j]);
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Name = "button" + (i + 1) + "," + (j + 1);
+                    buttons[i, j].Height = 100;
+                    buttons[i, j].Width = 100;
+                    buttons[i, j].Location = new Point(105 * (i + 1), 105 * (j + 1));
+                    //buttons[i,j].Text = "button " + (i + 1) + "," + (j + 1);
+                    buttons[i, j].Text = Convert.ToString((i + 1) + "," + (j + 1));
+                    buttons[i, j].Click += new EventHandler(this.butt_Click);
+                    buttons[i, j].BackColor = Color.MediumTurquoise;
+                    this.Controls.Add(buttons[i, j]);    
                 }
             }
-            start = new Button();
-            start.Location = new Point(210,450);
-            start.Height = 50;
-            start.Width = 100;
-            start.Text = "Start";
-            start.Click += new EventHandler(this.Start_Game);
-            this.Controls.Add(start);
-        }
-
-        public void gamePlay()
-        {
-            Console.WriteLine("Start of gamePlay again");
-            createSequence();
-            flashing(sequence);
-            Console.WriteLine("flashed");
-        }
-
-        public void createSequence()
-        {
             sequence.Add(new Tuple<int, int>(r.Next(3), r.Next(3)));
+
+            //Creats our timer and it's events
+            t.Interval = 5000;
+            t2.Interval = 7500;
+            t.Enabled = true;
+            t2.Enabled = true;
+            t.Elapsed += TimerEvent;
+            t2.Elapsed += TimerEvent2;
+
         }
 
-        public void flashing(List<Tuple<int,int>> sequence){
-            for (int i = 0; i < sequence.Count;i++)
-            {
-                b[sequence[i].Item1, sequence[i].Item2].BackColor = Color.Red;
-                Task.Delay(5000).Wait();
-                b[sequence[i].Item1, sequence[i].Item2].BackColor = Color.MediumTurquoise;
-            }
+            
 
-        }
-
-
-        //public void wait(int NoSecs)
-        //{
-        //    DateTime timeToWait = DateTime.Now.AddSeconds(NoSecs);
-        //    while (DateTime.Now < timeToWait)
-        //    {
-        //        Console.WriteLine("waiting..");
-        //    }
-        //    Console.WriteLine("done waiting");
-        //}
-
-
-        private void Start_Game(object sender, EventArgs e)
-        {
-            gamePlay();
-        }
-       
-
-        private void Button_Click(object sender, EventArgs e)
+        void butt_Click(object sender, EventArgs e)
         {
             Console.WriteLine(((Button)sender).Text);
-            for (int i = 0; i < sequence.Count; i++)
+
+            if (((Button)sender) == buttons[sequence[0].Item1, sequence[0].Item2])
             {
-                if (((Button)sender) == b[sequence[(sequence.Count - 1)].Item1, sequence[(sequence.Count - 1)].Item2])
-                {
-                    Console.WriteLine("HIT!");
-                    score = score + 1;
-                    Console.WriteLine(score);
-                    gamePlay();
-                }
-                else if (((Button)sender) == b[sequence[i].Item1, sequence[i].Item2])
-                {
-                    Console.WriteLine("HIT!");
-                }
-                else
-                {
-                    Console.WriteLine("MISS!");
-                }
+                //buttons[r.Next(3), r.Next(3)].BackColor = Color.Red;
+                Console.WriteLine("HIT!");
             }
+            else
+                Console.WriteLine("MISSED!");
         }
 
         private void TimerEvent(object sender, EventArgs e)
         {
-            for (int k = 0; k < sequence.Count; k++)
+            foreach (var element in sequence)
             {
-                b[sequence[k].Item1, sequence[k].Item2].BackColor = Color.Red;
-                Thread.Sleep(5000);
-                b[sequence[k].Item1, sequence[k].Item2].BackColor = Color.MediumTurquoise;
+                buttons[sequence[0].Item1, sequence[0].Item2].BackColor = Color.Red;
             }
-        }
-        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //About the program
+            //Stops our button from repeatedly turning Red by ending the timer.
+            t.Stop();
         }
 
-        private void howToPlayToolStripMenuItem1_Click(object sender, EventArgs e)
+        private void TimerEvent2(object sender, EventArgs e)
         {
-            //How to play
+            foreach (var element in sequence)
+            {
+                buttons[sequence[0].Item1, sequence[0].Item2].BackColor = Color.MediumTurquoise;
+            }
+            //Stops our button from repeatedly turning turquoise by ending the timer.
+            t2.Stop();
         }
+
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Close();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
+        }
     }
 }
